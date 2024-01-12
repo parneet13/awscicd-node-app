db3502a9e751da5b5aac9c897e71f71fde679b99
parneet13 <psmalkpuria@gmail.com>
2024-01-12 14:22:54 +0530

    add code

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#sudo apt-get update
-sudo rm -rf /var/www/html/node_modules
-cd /var/www/html
-npm i -f 
+#sudo rm -rf /var/www/html/node_modules
+#cd /var/www/html
+#npm i -f 
 #sleep 10
 #cd /var/www/html
 #npm start
