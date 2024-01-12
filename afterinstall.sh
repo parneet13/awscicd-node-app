9bde57f588a441fa9512328b8bdc426117b16d04
parneet13 <psmalkpuria@gmail.com>
2024-01-12 14:55:25 +0530

    add code

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
+#sudo apt-get update
 #sudo rm -rf /var/www/html/node_modules
 #cd /var/www/html
 #npm i -f 
-#sleep 10
-#cd /var/www/html
-#npm start
-#sudo apt install nginx
-#sudo apt install npm -y
